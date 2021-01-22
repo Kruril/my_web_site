ce9166e9a5cf731cbc16fe90ab864179bd8d45e1
Kruril <guillaume.charlier.2017@gmail.com>
2021-01-22 17:30:29 +0100

    add tailwind css config

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+export default function NavBar() {
+    return (
+        <div
+            className="z-50 sticky top-0 p-4 items-center bg-black bg-opacity-80 flex border-b-2 text-white"
+        >
+            <div className="flex-grow text-lg">
+                Bienvenue sur mon site web
+            </div>
+        </div>
+    );
+}
